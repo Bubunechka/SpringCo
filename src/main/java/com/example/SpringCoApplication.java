d5228f07d13b88ccca3dbf689773099c4001c144
Алёна Катанская <alyona.katanskaya@gmail.com>
2024-03-04 16:33:25 +0300

    рабочий security с логином и паролем(примитивный)

---
+++ src/main/java/com/example/SpringCoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoApplication {
+
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(SpringCoApplication.class, args);
+	}
+
+}
